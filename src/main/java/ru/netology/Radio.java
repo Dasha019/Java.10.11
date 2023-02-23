0ec7706e812b0eecdcc2e2298cd7dcbfd7ee1d70
Dasha Kozyulya <w.vnt0d@gmail.com>
2023-02-23 15:36:47 +0500

    init

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,57 @@
+package ru.netology;
+
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 100) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void nextStation() {
+        if (currentStation < 9) {
+            currentStation++;
+            return;
+        }
+            currentStation = 0;
+        }
+
+    public void prevStation() {
+        if (currentStation > 0) {
+            currentStation--;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+}
